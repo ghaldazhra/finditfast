07ae4abd493c76bbd42f684cb95c45a6206cf383
ghaldazhra <ghaldazhra1@gmail.com>
2026-02-13 14:47:48 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,83 @@
+//import logo from './logo.svg';
+import './App.css';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import Box from '@mui/material/Box';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span>
+          FandItFast
+          <Box sx={{
+                display: 'flex',
+                gap: 2,
+              }}
+            >
+              <Button variant="contained">Report Item</Button>
+              <Button variant="contained">View Items</Button>
+              <Button variant="contained">Admin Portal</Button>
+            </Box>
+        </span>
+        <section>
+          <div>
+            <h1>Lost & Found <br/>
+            Management System</h1>
+            <p>Report, track, and claim lost items quickly and efficiently. Your one-stop <br/>
+            solution for reuniting with your belongings.</p>
+            <Box sx={{
+                display: 'flex',
+                gap: 2,
+                justifyContent: 'center'
+              }}
+            >
+              <Button variant="contained">Report Lost Item</Button>
+              <Button variant="contained">View Lost Items</Button>
+            </Box>
+          </div>
+          <h2>How It Works</h2>
+          <p>A simple three-step process to report and recover lost items.</p>
+           <Box sx={{
+                display: 'flex',
+                gap: 2,
+                justifyContent: 'center'
+              }}
+            >
+              <Card variant="outlined" sx={{ padding: 2 }}>
+                <div>
+                  <h3>Step 1</h3>
+                  <div>Report</div>
+                  <p>Fill out a simple form <br/>
+                   to report your <br/>
+                   lost item.</p>
+                </div>
+              </Card>
+
+              <Card variant="outlined" sx={{ padding: 2 }}>
+                <div>
+                  <h3>Step 2</h3>
+                  <div>Verify</div>
+                  <p>Our Team verifies the <br/>
+                   report and updates the <br/>
+                   item's status.</p>
+                </div>
+              </Card>
+
+              <Card variant="outlined" sx={{ padding: 2 }}>
+                <div>
+                  <h3>Step 3</h3>
+                  <div>Claim</div>
+                  <p>Once found you'll be <br/>
+                   notified and can claim<br/>
+                   your item.</p>
+                </div>
+              </Card>
+            </Box>
+        </section>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ReportItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+export default function ReportItem() {
+
+  return <div>Add the router.</div>
+}
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    )
+}
